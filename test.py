3101d95e0dd8098e70cdb3512be38c544a3b9228
UP-4303 <up4303.pro@protonmail.com>
2023-10-24 16:30:18 +0200

    sac

--- test.py
+++ test.py
@@ -1,11 +1,34 @@
 import requests
+import obspy
+import zipfile as zf
+from os import listdir
 
-def generateRequest():
+def sendRequest():
     baselink = "http://service.iris.edu/fdsnws/dataselect/1/query"
+    params = {
+        "format":"sac.zip",
+        "start":"2010-02-27T06:30:00.000",
+        "end":"2010-02-27T10:30:00.000",
+        "net":"IU",
+        "sta":"ANMO",
+        "loc":"00",
+        "cha":"BHZ"
+    }
+    return requests.get(baselink, params)
 
 if __name__ == "__main__":
-    r = requests.get("http://service.iris.edu/fdsnws/dataselect/1/query?net=IU&sta=ANMO&loc=00&cha=BHZ&start=2010-02-27T06:30:00.000&end=2010-02-27T10:30:00.000")
+    filebasename = "IU.ANMO.00.BHZ.M.2010.058.063000"
+    filename = filebasename + ".SAC"
+    zipname = filebasename + '.sac.zip'
+
+    r = sendRequest()
     print(r.status_code)
 
-    with open('test.mseed', 'wb') as file:
+    with open(zipname, 'wb') as file:
         file.write(r.content)
+
+    with zf.ZipFile(zipname, 'r') as zip_ref:
+        zip_ref.extractall('test')
+
+    st = obspy.read("test\\"+ listdir("test")[0])
+    st.plot()
