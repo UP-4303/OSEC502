8bb4feb66e1533064273359870bdfd30235804ba
UP-4303 <up4303.pro@protonmail.com>
2023-11-27 15:47:40 +0100

    séparation fichiers

--- main.py
+++ main.py
@@ -1,57 +1,17 @@
-import requests
 import obspy
 import zipfile as zf
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
 from tabulate import tabulate
 import os
 
 from sacToWav import sacToWav
+from distance import distance
+from sage_requests import *
+from convertions import geoCSVToDictArray
 
-now = datetime.now()
 isoDatetimeFormat = '%Y-%m-%dT%H:%M:%SZ'
 stationDatetimeFormat = '%Y-%m-%dT%H:%M:%S.%f'
 fileDatetimeFormat = '%Y.%m.%d.%H.%M.%S'
-
-def requestLastEvents(howMany):
-	baselink = "https://service.iris.edu/fdsnws/event/1/query"
-	params = {
-		"limit":str(howMany),
-		"orderby":"time",
-		"format":"geocsv",
-		"start":str((now - relativedelta(years=2)).date()),
-		"end":str(now.date())
-	}
-	return requests.get(baselink, params)
-
-def requestNearestStations(lat, lon, eventTime):
-	baselink = "https://service.iris.edu/fdsnws/station/1/query"
-	params = {
-		"lat":str(lat),
-		"lon":str(lon),
-		"maxradius":20,
-		"startbefore":str((eventTime - relativedelta(minute=5)).date()),
-		"endafter":str((eventTime + relativedelta(minute=5)).date()),
-		"format":"geocsv"
-	}
-	return requests.get(baselink, params)
-
-def requestSac(networkId, stationId, eventTime):
-	baselink = "http://service.iris.edu/fdsnws/dataselect/1/query"
-	params = {
-		"format":"sac.zip",
-		"start":str((eventTime - relativedelta(minute=5)).date()),
-		"end":str((eventTime + relativedelta(minute=5)).date()),
-		"net":networkId,
-		"sta":stationId
-	}
-	return requests.get(baselink, params)
-
-def requestMultipleSacs(stations, eventTime):
-	requests = []
-	for i in range(len(stations)):
-		requests.append(requestSac(stations[i]["Network"], stations[i]["Station"], eventTime))
-	return requests
 
 def saveFile(content, path):
 	with open(path, "wb") as file:
@@ -63,33 +23,7 @@ def unzip(path, destPath):
 		zip_ref.extractall(destPath, members=[result])
 	return result
 
-def parseGeoCsvString(string: str):
-	res = []
-	lines = string.split("\n")
-
-	for i in range(len(lines)):
-		if(not lines[i].startswith('#')):
-			res.append(lines[i].split('|'))
-	return res[:-1]
-
-def csvArrayToDictArray(csvArray):
-	if(len(csvArray) <= 1):
-		return []
-
-	res = []
-	headers = csvArray[0]
-	csvArray = csvArray[1:]
-
-	for i in range(len(csvArray)):
-		res.append({})
-		for j in range(len(headers)):
-			res[i][headers[j]] = csvArray[i][j]
-	return res
-
-def geoCSVToDictArray(string: str):
-	return csvArrayToDictArray(parseGeoCsvString(string))
-
-def selectEventMenuCLI(lastEvents: [any]):
+def printDict(lastEvents: [any]):
 	print(tabulate(lastEvents, headers="keys", showindex="always"))
 
 def main():
@@ -110,7 +44,7 @@ def main():
 		print("ERROR, LAST EVENTS REQUEST FAILED... STATUS :", r.status_code)
 		exit()
 	lastEvents = geoCSVToDictArray(r.content.decode("utf-8"))
-	selectEventMenuCLI(lastEvents)
+	printDict(lastEvents)
 	
 	eventSelected = int(input(f"Choose an event (between 0 and {howManyEvents-1}, -1 to quit) : "))
 	while(eventSelected < -1 and eventSelected >= howManyEvents):
@@ -129,7 +63,7 @@ def main():
 	)
 	stations = geoCSVToDictArray(r.content.decode("utf-8"))[:howManyStations]
 
-	selectEventMenuCLI(stations)
+	printDict(stations)
 
 	print("GETTING SAC FILES")
 	rs = requestMultipleSacs(stations, eventTime)
