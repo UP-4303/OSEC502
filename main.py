093e4547bd73114243e0943cb305dc616bb84545
UP-4303 <up4303.pro@protonmail.com>
2023-11-27 15:37:56 +0100

    requêtes des évenements, stations et sacs

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 import requests
 import obspy
 import zipfile as zf
-from sacToWav import sacToWav
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from tabulate import tabulate
+import os
+
+from sacToWav import sacToWav
+
+now = datetime.now()
+isoDatetimeFormat = '%Y-%m-%dT%H:%M:%SZ'
+stationDatetimeFormat = '%Y-%m-%dT%H:%M:%S.%f'
+fileDatetimeFormat = '%Y.%m.%d.%H.%M.%S'
 
 def requestLastEvents(howMany):
-	now = datetime.now()
-
 	baselink = "https://service.iris.edu/fdsnws/event/1/query"
 	params = {
 		"limit":str(howMany),
 		"orderby":"time",
 		"format":"geocsv",
-		"start":str(now.replace(year=now.year-2).date()),
+		"start":str((now - relativedelta(years=2)).date()),
 		"end":str(now.date())
 	}
 	return requests.get(baselink, params)
 
-def requestSac():
+def requestNearestStations(lat, lon, eventTime):
+	baselink = "https://service.iris.edu/fdsnws/station/1/query"
+	params = {
+		"lat":str(lat),
+		"lon":str(lon),
+		"maxradius":20,
+		"startbefore":str((eventTime - relativedelta(minute=5)).date()),
+		"endafter":str((eventTime + relativedelta(minute=5)).date()),
+		"format":"geocsv"
+	}
+	return requests.get(baselink, params)
+
+def requestSac(networkId, stationId, eventTime):
 	baselink = "http://service.iris.edu/fdsnws/dataselect/1/query"
 	params = {
 		"format":"sac.zip",
-		"start":"2010-02-27T06:30:00.000",
-		"end":"2010-02-27T10:30:00.000",
-		"net":"IU",
-		"sta":"ANMO",
-		"loc":"00",
-		"cha":"BHZ"
+		"start":str((eventTime - relativedelta(minute=5)).date()),
+		"end":str((eventTime + relativedelta(minute=5)).date()),
+		"net":networkId,
+		"sta":stationId
 	}
 	return requests.get(baselink, params)
+
+def requestMultipleSacs(stations, eventTime):
+	requests = []
+	for i in range(len(stations)):
+		requests.append(requestSac(stations[i]["Network"], stations[i]["Station"], eventTime))
+	return requests
 
 def saveFile(content, path):
 	with open(path, "wb") as file:
@@ -36,79 +59,112 @@ def saveFile(content, path):
 
 def unzip(path, destPath):
 	with zf.ZipFile(path) as zip_ref:
-		zip_ref.extractall(destPath)
+		result = zip_ref.namelist()[0]
+		zip_ref.extractall(destPath, members=[result])
+	return result
 
 def parseGeoCsvString(string: str):
 	res = []
 	lines = string.split("\n")
 
 	for i in range(len(lines)):
-		res.append(lines[i].split('|'))
-	return res[4:-1]
+		if(not lines[i].startswith('#')):
+			res.append(lines[i].split('|'))
+	return res[:-1]
 
-def selectEventMenuCLI(lastEvents: [any], dontPrintLabels: [str] = ['Author', 'Catalog', 'Contributor', 'ContributorId']):
-	dontPrintIndex = []
-	# Unwanted columns
-	for i in range(0, len(lastEvents[0])):
-		if(lastEvents[0][i] in dontPrintLabels):
-			dontPrintIndex.append(i)
+def csvArrayToDictArray(csvArray):
+	if(len(csvArray) <= 1):
+		return []
 
-	if(len(lastEvents) > 0):
-		charsMax = [0]*len(lastEvents[0])
+	res = []
+	headers = csvArray[0]
+	csvArray = csvArray[1:]
 
-	# Maximum characters/column for tabulation
-	for i in range(0, len(lastEvents)):
-		for j in range(0, len(lastEvents[i])):
-			charsMax[j] = max(charsMax[j], len(lastEvents[i][j]))
+	for i in range(len(csvArray)):
+		res.append({})
+		for j in range(len(headers)):
+			res[i][headers[j]] = csvArray[i][j]
+	return res
 
-	# Print
-	for i in range(0, len(lastEvents)):
-		for j in range(0, len(lastEvents[i])):
-			if(not j in dontPrintIndex):
-				print(lastEvents[i][j], end=("\t"*((charsMax[j]-len(lastEvents[i][j]))//4+1)))
-		print('')
+def geoCSVToDictArray(string: str):
+	return csvArrayToDictArray(parseGeoCsvString(string))
+
+def selectEventMenuCLI(lastEvents: [any]):
+	print(tabulate(lastEvents, headers="keys", showindex="always"))
 
 def main():
 	print("STARTING...")
-	filebasename = "IU.ANMO.00.BHZ.M.2010.058.063000"
-	filename = filebasename + ".sac"
-	zipname = filebasename + ".sac.zip"
-	wavfilename = filebasename + ".wav"
+
+	filename = lambda filebasename: filebasename + ".sac"
+	zipname = lambda filebasename: filebasename + ".sac.zip"
+	wavfilename = lambda filebasename: filebasename + ".wav"
+	
+	generatefilebasename = lambda s: f"{s['Network']}.{s['Station']}.{datetime.strftime(datetime.strptime(s['StartTime'], stationDatetimeFormat), fileDatetimeFormat)}"
 
 	howManyEvents = 5
+	howManyStations = 2
 
 	print("GETTING LAST EVENTS")
 	r = requestLastEvents(howManyEvents)
 	if(r.status_code != 200):
 		print("ERROR, LAST EVENTS REQUEST FAILED... STATUS :", r.status_code)
-		exit
-	lastEvents = parseGeoCsvString(r.content.decode("utf-8"))
+		exit()
+	lastEvents = geoCSVToDictArray(r.content.decode("utf-8"))
 	selectEventMenuCLI(lastEvents)
+	
+	eventSelected = int(input(f"Choose an event (between 0 and {howManyEvents-1}, -1 to quit) : "))
+	while(eventSelected < -1 and eventSelected >= howManyEvents):
+		eventSelected = int(input(f"Please select a valid event !\nChoose an event (between 0 and {howManyEvents-1}, -1 to quit) : "))
+	if(eventSelected == -1):
+		exit()
 
-	print("GETTING SAC FILE")
-	r = requestSac()
-	if(r.status_code != 200):
-		print("ERROR, SAC FILE REQUEST FAIL... STATUS :", r.status_code)
-		exit
+	eventTime = datetime.strptime(lastEvents[eventSelected]['Time'], isoDatetimeFormat)
+	
+	generateWav = input("GENERATE WAV FILES ? [y/n] ") == 'y'
 
-	print("SAVING ZIP FILE")
-	saveFile(r.content, "saczip/"+zipname)
+	r = requestNearestStations(
+		lastEvents[eventSelected]['Latitude'],
+		lastEvents[eventSelected]['Longitude'],
+		eventTime
+	)
+	stations = geoCSVToDictArray(r.content.decode("utf-8"))[:howManyStations]
 
-	print("UNZIPPING")
-	unzip("saczip/"+zipname, "sac")
+	selectEventMenuCLI(stations)
 
-	print("OPENING")
-	st = obspy.read("sac/"+filename)
+	print("GETTING SAC FILES")
+	rs = requestMultipleSacs(stations, eventTime)
+	resultArray = []
 
-	if(input("GENERATE WAV FILE ? [y/n]") == 'y'):
-		print("GENERATING WAV FILE")
-		sacToWav(st, 'wav/'+wavfilename)
+	for fileToDelete in os.listdir('sac'):
+		os.remove(os.path.join('sac', fileToDelete))
 
-	print("PLOTING")
-	st.plot()
+	for i in range(len(rs)):
+		print(f"PROCESSING INDEX {i}")
+
+		if(rs[i].status_code != 200):
+			print("ERROR, SAC FILE REQUEST FAIL... STATUS :", rs[i].status_code)
+			if(rs[i].status_code == 204):
+				print("IT SEEMS THAT THIS STATION WAS UP BUT NOTHING WAS RECORDED...")
+			print(f"STATION WAS {stations[i]['Station']}, LOCATED AT LAT: {stations[i]['Latitude']}, LON: {stations[i]['Longitude']}")
+			print(f"REQUEST: {rs[i].request.url}")
+			print(f"RESULT: {rs[i].content}")
+		else:
+			print("SAVING ZIP FILE")
+			filebasename = generatefilebasename(stations[i])
+			
+			saveFile(rs[i].content, "saczip/" + zipname(filebasename))
+
+			print("UNZIPPING")
+			tempFileName = unzip("saczip/" + zipname(filebasename), "sac")
+
+			os.rename(os.path.join("sac", tempFileName), os.path.join("sac", filename(filebasename)))
+
+			print("OPENING")
+			resultArray.append(obspy.read("sac/" + filename(filebasename)))
+
+			if (generateWav):
+				print("GENERATING WAV FILE")
+				sacToWav(resultArray[-1], 'wav/'+ wavfilename(filebasename))
 
 if __name__ == "__main__":
-	r = requestLastEvents(5)
-	print(r.status_code)
-	lastEvents = parseGeoCsvString(r.content.decode("utf-8"))
-	selectEventMenuCLI(lastEvents)
+	main()
